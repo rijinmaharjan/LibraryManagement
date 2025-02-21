249728090ba768d75615aff9d70e4c404e92d819
rijinmaharjan <mhzn.rijin8@gmail.com>
2025-02-21 21:33:55 +0545

    full size window

--- loginpage.py
+++ loginpage.py
@@ -1,1 +1,26 @@
 from tkinter import *
+from tkinter import messagebox
+import sqlite3
+
+#for signin and signup initializing database
+logindata = sqlite3.connect('users.db')
+cursor=logindata.cursor()
+cursor.execute(
+    '''CREATE TABLE IF NOT EXISTS users(
+    username text PRIMARYKEY,
+    password text NOT NULL
+    )
+    '''
+)
+logindata.commit()
+
+login=Tk()
+login.title('Library Management System Login')
+login.attributes("-fullscreen",True)
+
+
+
+def exit_fullscreen(event):
+    login.attributes("-fullscreen",False)
+login.bind('<Escape>',exit_fullscreen)
+login.mainloop()
